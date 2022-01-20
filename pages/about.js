b486871704882361fe170b53a0518334fdbccc52
Blaine Curren <blaine.curren@gmail.com>
2022-01-20 11:27:02 -0600

    updated navbar and cart counter

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const about = () => {
+    return (
+        <div className="asd">
+            
+        </div>
+    )
+}
+
+export default about
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mel's One Stop Shop</title>
+        <meta name="description" content="We've got everything you need!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Homepage
+    </div>
+  );
+}
