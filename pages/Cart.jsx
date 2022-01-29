4dd6fe77cf727db68d2f90beed3336aabcc18d31
Blaine Curren <blaine.curren@gmail.com>
2022-01-29 15:15:01 -0600

    added cart table to cart.jsx

---
+++ pages/Cart.jsx
@@ -0,0 +1,26 @@
+import styles from "../styles/Cart.module.css";
+import Image from "next/image";
+
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.right}></div>
+    </div>
+  );
+};
+
+export default Cart;
