627b12d6ff489fdde6f7ec8d37764392258e9d81
Blaine Curren <blaine.curren@gmail.com>
2022-01-30 12:24:56 -0600

    started order tracking page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "06d0ec67cd155288"; }
+/******/ 		__webpack_require__.h = function() { return "eabd5e17be8897ce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/orders/[id].jsx
@@ -0,0 +1,49 @@
+import styles from "../../styles/Order.module.css";
+import Image from "next/image";
+
+const Order = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.row}>
+          <table className={styles.table}>
+            <tr className={styles.tr}>
+              <th>Order ID</th>
+              <th>Customer</th>
+              <th>Address</th>
+              <th>Total</th>
+            </tr>
+            <tr>
+              <td>
+                  <span className={styles.id}></span>
+              </td>
+              <td>
+                  <span></span>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button disabled className={styles.button}>
+            PAID
+          </button>
+        </div>
+        <div className={styles.row}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
